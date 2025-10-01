8b7112cb3abce7704d0f11348cd6c83b984ccec1
Ilya <ilyafeok228@gmail.com>
2025-10-01 17:24:50 +0700

    ads

--- task_1.py
+++ task_1.py
@@ -5,6 +5,11 @@ entered_number = 0
 attempts = 0
 while random_number != entered_number:
     print('Введите число от 1 до 20')
-    entered_number = int(input())
     attempts += 1
+    try:
+        entered_number = int(input())
+    except:
+        attempts -= 1
+        entered_number = 0
+        print("Введите правильно число")
 print(f'Ура! Вы отгадали число за {attempts} попыток!')
